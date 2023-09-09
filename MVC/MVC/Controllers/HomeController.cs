2d2b0fc2403038c0d3066f1d439eaa4e3be486ae
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-09-09 20:55:00 +0300

    ASP

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Model;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext db;
+        public HomeController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await db.users.ToListAsync();
+            return View(users);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> About(Users user)
+        {
+            db.users.Add(user);
+            await db.SaveChangesAsync();
+            return View("Index");
+        }
+    }
+}
